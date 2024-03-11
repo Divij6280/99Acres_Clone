5815cc192d2355fd0c21fafbc0f504c65b85f04a
Divij6280 <120786175+Divij6280@users.noreply.github.com>
2024-03-11 22:27:27 +0530

    Update main.js

--- ShelterSeek/main.js
+++ ShelterSeek/main.js
@@ -17,13 +17,66 @@ microphoneIcon.addEventListener('click', (e) => {
 
 //  +++++++++++++++++++++++++  NEW DIV ON HOVER ON (FOR BUYERS) IN NAVIGATION   +++++++++++++++++++++++++++
 
-// const hoverTarget = document.querySelector('.for_buyers');
-// const hoverContent = document.querySelector('.for_buyers_hover');
+const hoverBuyers = document.querySelector('.for_buyers');
+const hoverContent1 = document.querySelector('.for_buyers_hover');
 
-// hoverTarget.addEventListener('mouseover', (e) => {
-//   hoverContent.style.display = 'block';
-// });
+// Event listeners for both hoverBuyers and hoverContent
+hoverBuyers.addEventListener('mouseover', () => {
+    hoverContent1.style.display = 'flex';
+});
 
-// hoverTarget.addEventListener('mouseout', (e) => {
-//   hoverContent.style.display = 'none';
-// });
+hoverContent1.addEventListener('mouseover', () => {
+    hoverContent1.style.display = 'flex';
+});
+
+hoverBuyers.addEventListener('mouseout', () => {
+    hoverContent1.style.display = 'none';
+});
+
+hoverContent1.addEventListener('mouseout', () => {
+    hoverContent1.style.display = 'none';
+});
+
+
+//  +++++++++++++++++++++++++  ON CLICKING BUY RENT etc.. IN SEARCHBAR   +++++++++++++++++++++++++++
+
+const h5 = document.querySelector('#searchbar_2_div_change_h5');
+const buy = document.querySelector('#buy_searchbar_1');
+const rent = document.querySelector('#rent_searchbar_1');
+const new_launch = document.querySelector('#new_launch_searchbar_1');
+const pg = document.querySelector('#pg_searchbar_1');
+const commercial = document.querySelector('#commercial_searchbar_1');
+const plots = document.querySelector('#plots_searchbar_1');
+const projects = document.querySelector('#projects_searchbar_1');
+
+
+
+//Event listeners
+
+buy.addEventListener('click' , () => {
+    h5.innerHTML = "Buy Now"
+});
+
+rent.addEventListener('click' , () => {
+  h5.innerHTML = "Rent now"
+});
+
+new_launch.addEventListener('click' , () => {
+  h5.innerHTML = "Newly Launched"
+});
+
+pg.addEventListener('click' , () => {
+  h5.innerHTML = "PG/Co-living"
+});
+
+commercial.addEventListener('click' , () => {
+  h5.innerHTML = "All Residentials"
+});
+
+plots.addEventListener('click' , () => {
+  h5.innerHTML = "Find Plots/Lands"
+});
+
+projects.addEventListener('click' , () => {
+  h5.innerHTML = "Residential Project"
+});
